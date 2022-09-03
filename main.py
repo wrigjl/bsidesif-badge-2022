3289ad4c4b6a666dd2d0f7942a710c58064c5d51
Todd Keller <spectre@bsidesif.org>
2022-09-03 17:38:50 -0600

    added example for contributed functionality

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import funcs
 import time
+import spectre
 
-funcs.conn()
-countz = 0
-while True:
-    print(f'Loop iteration {countz}')
-    funcs.npix_ran(funcs.np)
-    time.sleep(.25)
-    countz += 1
+
+def main_func():
+    funcs.conn()
+
+def test_func():
+    countz = 0
+    while True:
+        print(f'Loop iteration {countz}')
+        funcs.npix_ran(funcs.np)
+        time.sleep(.25)
+        countz += 1
+
+
+if __name__ == "__main__":
+   print("Executed when ran directly")
+   main_func()
+else:
+   print("Executed when imported")
---
+++ spectre.py
@@ -0,0 +1,7 @@
+import time
+
+def boot_init():
+    print(f'Spectre Init Success')
+
+def main_loop():
+    print(f'Spectre Loop Success')

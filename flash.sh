3fa0b1030a9160b4baf9fca469978dabe68787bb
phakt0r <factor@islug.org>
2022-09-14 10:59:35 -0600

    Added instructions to flash script.

--- flash.sh
+++ flash.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+echo "
+*************************************************************
+***                                                       ***
+***  To flash the board, you will need to rest the board  ***
+***  into bootmode 3 times. to do this unplug the board   ***
+***  and while holding the button, plug the board back in ***
+***  You will be promped when to do that.                 ***
+***                                                       ***
+***  if you have a USB hub that has a switch on it, that  ***
+***  may be helfull here.                                 ***
+***                                                       ***
+*************************************************************"
 
+echo "******** CHIP ID **********"
+read -n 1 -s -r -p "Please reset board now, after press any key to continue"
 esptool.py --port /dev/ttyUSB0 chip_id
+echo "******** Erase Flash **********"
+read -n 1 -s -r -p "Please reset board now, after press any key to continue"
 esptool.py --port /dev/ttyUSB0 erase_flash
+echo "******** FLASH CHIP **********"
+read -n 1 -s -r -p "Please reset board now, after press any key to continue"
 esptool.py --port /dev/ttyUSB0 --baud 460800 write_flash --flash_size=detect 0  ./firmware/esp8266-20220618-v1.19.1.bin

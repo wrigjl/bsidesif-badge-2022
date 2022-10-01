4d1d8cc12bb06e08127ef623986a4c3ce84eed55
Spectre03 <spectre03@gmail.com>
2022-10-01 16:07:42 -0600

    update push.sh to take command line args

--- push.sh
+++ push.sh
@@ -1,15 +1,48 @@
 #!/bin/bash
 
-DEVICE=$1
+DEVICE='/dev/ttyUSB0'
+Json='false'
+DFLAG='false'
 
-echo "Starting copy"
+while getopts 'jf:' flag
+do
+    case "${flag}" in
+        f)
+        DEVICE=$OPTARG
+        DFLAG='true'
+        ;;
+        j) Json='true' ;;
+        ?)
+        echo "Bad arguments supplied"
+        echo "Run as push.sh <-j>  -f [DEVICE_HANDLE]"
+        exit 1
+        ;;
+    esac
+done
+shift "$(($OPTIND -1))"
+
+if [ "$Json" = true ]
+  then
+    echo "Pushing tokens.json to $DEVICE"
+    ampy --port $DEVICE put tokens.json
+  else
+    echo "skipped tokens.json"
+fi
+
+if [ "$DFLAG" = false ]
+  then
+    echo "No device supplied"
+    echo "Running with default device handle of $DEVICE"
+fi
+
+echo "Starting copy to $DEVICE"
 ampy --port $DEVICE put funcs.py
 ampy --port $DEVICE put api.py
 ampy --port $DEVICE put pixel.py
-#ampy --port /dev/tty.usbserial-10 put tokens.json  # Uncomment first time you push, then re-comment
+# ampy --port $DEVICE put tokens.json  # Uncomment first time you push, then re-comment
 # alternatively you can hand-run that command once
 ampy --port $DEVICE put main.py
 ampy --port $DEVICE put spectre.py
 ampy --port $DEVICE put secrets.py
-echo "Now copying primitives... may take a while"
-ampy --port $DEVICE put primitives
+# echo "Now copying primitives... may take a while"
+# ampy --port $DEVICE put primitives

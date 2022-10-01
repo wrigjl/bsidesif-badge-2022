21a4f1a2d41c70385c0b1ffa49a7da7199272626
Meecles <Meecles@users.noreply.github.com>
2022-10-01 18:02:48 -0400

    Ooo

--- push.sh
+++ push.sh
@@ -6,10 +6,8 @@ echo "Starting copy"
 ampy --port $DEVICE put funcs.py
 ampy --port $DEVICE put api.py
 ampy --port $DEVICE put pixel.py
-#ampy --port /dev/tty.usbserial-10 put tokens.json  # Uncomment first time you push, then re-comment
+ampy --port $DEVICE put tokens.json  # Uncomment first time you push, then re-comment
 # alternatively you can hand-run that command once
 ampy --port $DEVICE put main.py
 ampy --port $DEVICE put spectre.py
 ampy --port $DEVICE put secrets.py
-echo "Now copying primitives... may take a while"
-ampy --port $DEVICE put primitives

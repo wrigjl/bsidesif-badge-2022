713da8717c1be5b3803e13fe76d44b9eaf96bbea
Spectre03 <spectre03@gmail.com>
2022-10-01 14:31:15 -0600

    Update push.sh file

--- push.sh
+++ push.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+DEVICE=$1
+
 echo "Starting copy"
-ampy --port /dev/tty.usbserial-10 put funcs.py
-ampy --port /dev/tty.usbserial-10 put api.py
-ampy --port /dev/tty.usbserial-10 put pixel.py
+ampy --port $DEVICE put funcs.py
+ampy --port $DEVICE put api.py
+ampy --port $DEVICE put pixel.py
 #ampy --port /dev/tty.usbserial-10 put tokens.json  # Uncomment first time you push, then re-comment
 # alternatively you can hand-run that command once
-ampy --port /dev/tty.usbserial-10 put main.py
-ampy --port /dev/tty.usbserial-10 put spectre.py
-ampy --port /dev/tty.usbserial-10 put secrets.py
+ampy --port $DEVICE put main.py
+ampy --port $DEVICE put spectre.py
+ampy --port $DEVICE put secrets.py
 echo "Now copying primitives... may take a while"
-ampy --port /dev/tty.usbserial-10 put primitives
+ampy --port $DEVICE put primitives
